5098b05108d1c4a7d3f40f49cf1a3c00d3d61579
Yuichiro021 <71137269+CristianCmeciu@users.noreply.github.com>
2025-04-05 00:08:00 +0300

    Added Generic API Calls

---
+++ JavaServer/src/main/java/org/example/APICalls.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class APICalls {
+    public static String request(String parameter) throws MalformedURLException {
+        try {
+            URL url = new URL("https://rest.kegg.jp/" + parameter);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Content-Language", "en-US");
+            conn.setUseCaches(false);
+            conn.setDoOutput(true);
+            conn.connect();
+
+            InputStream is = conn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            ArrayList<String> response = new ArrayList<>();
+            String inputLine;
+            int n = 1000;
+            while ((inputLine = reader.readLine()) != null && n!=0) {
+                response.add(inputLine);
+                n--;
+            }
+            reader.close();
+            return response.toString();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- JavaServer/src/main/java/org/example/Main.java
+++ JavaServer/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.net.MalformedURLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            APICalls.request("list/hsa");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
