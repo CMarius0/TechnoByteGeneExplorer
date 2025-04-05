247b4d786e0768caa23effea682438556c17ae79
CMarius0 <mariuscacior@gmail.com>
2025-04-05 18:56:47 +0300

    Ui now sends what gene it wants to query

--- ElectronClient/src/routes/main_window.tsx
+++ ElectronClient/src/routes/main_window.tsx
@@ -1,28 +1,36 @@
-import { Box, Button, TextField } from "@mui/material";
-//import { useState } from "react";
-import $ from 'jquery'
+import { Box, Button, TextField,  } from "@mui/material";
 
+import { useState } from "react";
+import $ from 'jquery';
 
-
-function call() {
-    $.ajax({
-        url: "http://localhost:1080",
-        data:{
-          key: "test"
-        },
-        success: function( result ) {
-          alert(result);
-        }
-      }); 
-    
-} 
 
 export default function MainWindow() {
-    //const [data, setData] = useState("");
+    const [data, setData] = useState({name:"" ,summary:""});
     return (
+      <>
         <Box>
-            <TextField label="test" variant="filled" />
-            <Button onClick={call}>Call</Button>
+            <TextField label="Gene" variant="filled" id='searchTextField' />
+            <Button variant="contained"
+              onClick={() => {
+              $.ajax({
+                url: "http://localhost:1080",
+                data:{
+                  type: "GetGene",
+                  gene: (document.getElementById("searchTextField") as HTMLInputElement).value
+                },
+                success: function( result ) {
+                  const gene = JSON.parse(result);
+                  setData(gene);
+                }
+              });
+            }}>Search</Button>
         </Box>
+        <Box sx={{flexDirection: "column"}}>
+          <div>
+            <p>{data.name}</p>
+            <p>{data.summary}</p>
+          </div>
+        </Box>
+      </>
     )
-}
+} 
