afc05243fce0911c0b00fa1669a2943af351ea0d
Yuichiro021 <71137269+CristianCmeciu@users.noreply.github.com>
2025-04-05 12:53:47 +0300

    Changed used API

--- JavaServer/src/main/java/org/example/APICalls.java
+++ JavaServer/src/main/java/org/example/APICalls.java
@@ -1,13 +1,14 @@
 package org.example;
 
+import org.json.JSONObject;
+
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
 public class APICalls {
-    public static ArrayList<String> request(String parameter) throws MalformedURLException {
+    public static ArrayList<String> request(String parameter) {
         try {
             URL url = new URL("https://rest.kegg.jp/" + parameter);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -23,16 +24,59 @@ public class APICalls {
             ArrayList<String> response = new ArrayList<>();
             String inputLine;
             int n = 1000;
-            while ((inputLine = reader.readLine()) != null && n!=0) {
+            while ((inputLine = reader.readLine()) != null && n != 0) {
                 response.add(inputLine);
                 n--;
             }
             reader.close();
             return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
-        catch (Exception e) {
+    }
+
+    public static Integer getGeneIdFromSymbol(String symbol) {
+        try {
+            URL url = new URL("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gene&term=" + symbol + "[gene]+AND+Homo+sapiens[orgn]&retmode=json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Content-Language", "en-US");
+            conn.setUseCaches(false);
+            conn.setDoOutput(true);
+            conn.connect();
+
+            InputStream is = conn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            JSONObject jsonObject = new JSONObject(reader.readLine());
+            return Integer.parseInt(jsonObject.getJSONObject("esearchresult").getJSONArray("idlist").get(0).toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static JSONObject getGeneInfoFromID(Integer id) {
+        try {
+            URL url = new URL("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=" + id + "&retmode=json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Content-Language", "en-US");
+            conn.setUseCaches(false);
+            conn.setDoOutput(true);
+            conn.connect();
+
+            InputStream is = conn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            JSONObject jsonObject = new JSONObject(reader.readLine());
+            JSONObject test = jsonObject.getJSONObject("result").getJSONObject(id.toString());
+            return test;
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 }
+
--- JavaServer/src/main/java/org/example/Main.java
+++ JavaServer/src/main/java/org/example/Main.java
@@ -5,17 +5,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            List<String> lista = APICalls.request("get/hsa:7293");
-            List<String> lista2 = APICalls.request("list/hsa");
-            if (lista != null)
-                for (var x: lista)
-                    System.out.println(x);
-            if (lista2 != null)
-                for (var x: lista2)
-                    System.out.println(x);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
+        System.out.println(APICalls.getGeneIdFromSymbol("TP53"));
+        System.out.println(APICalls.getGeneInfoFromID(APICalls.getGeneIdFromSymbol("TP53")));
+        /*
+        List<String> lista = APICalls.request("get/hsa:7293");
+        List<String> lista2 = APICalls.request("list/hsa");
+
+        if (lista != null)
+            for (var x: lista)
+                System.out.println(x);
+        if (lista2 != null)
+            for (var x: lista2)
+                System.out.println(x);
+         */
     }
 }
