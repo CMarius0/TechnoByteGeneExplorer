c015b8872d349dd4a57908d4facb0d8d07c49b52
Yuichiro021 <71137269+CristianCmeciu@users.noreply.github.com>
2025-04-05 13:39:01 +0300

    Created Server

---
+++ JavaServer/src/main/java/org/example/Server.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Listening");
+        try(ServerSocket server = new ServerSocket(1080)) {
+            Socket s = server.accept();
+            BufferedReader bis = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            BufferedOutputStream bos = new BufferedOutputStream(s.getOutputStream());
+
+            System.out.println("Connected");
+            JSONObject obj = new JSONObject(bis.read());
+
+            if (obj.get("key") == "test") {
+                JSONObject send = new JSONObject();
+                send.append("test","hielau");
+                bos.write(obj.toString().getBytes());
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
